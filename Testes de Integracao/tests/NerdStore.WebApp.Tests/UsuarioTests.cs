21a008be45e872158c0e33252210dc2ad2039f4d
matheus-hr <matheushenri4141@gmail.com>
2024-09-26 11:22:16 -0300

    Inclusão de testes de cadastro inválido, login e criação da classe de prioridade de testes

--- Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using NerdStore.WebApp.Tests.Config;
+﻿using NerdStore.WebApp.Tests.Config;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,6 +6,7 @@ using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [TestCaseOrderer("NerdStore.WebApp.Tests.Config.PriorityOrderer", "NerdStore.WebApp.Tests")] //Vem da classe PriorityOrderer.cs na pasta config
     [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTests
     {
@@ -17,8 +17,7 @@ namespace NerdStore.WebApp.Tests
             _testsFixture = testsFixture;
         }
 
-
-        [Fact(DisplayName = "Realizar cadastro com sucesso")]
+        [Fact(DisplayName = "Realizar cadastro com sucesso"), TestPriority(1)]
         [Trait("Categoria", "Integração Web - Usuário")]
         public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
         {
@@ -48,6 +47,75 @@ namespace NerdStore.WebApp.Tests
             //Assert
             var responseString = await postResponse.Content.ReadAsStringAsync();
             
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {_testsFixture.UsuarioEmail}!", responseString);
+        }
+
+        [Fact(DisplayName = "Realizar cadastro senha fraca"), TestPriority(3)]
+        [Trait("Categoria", "Integração Web - Usuário")]
+        public async Task Usuario_RealizarCadastroComSenhaFraca_DeveRetornarMensagemDeErro()
+        {
+            var initialResponse = await _testsFixture.Client.GetAsync(requestUri: "/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
+            _testsFixture.GerarUserSenha();
+            const string senhaFraca = "123456";
+
+            var formData = new Dictionary<string, string>
+            {
+                { _testsFixture.AntiForgeryFieldName, antiForgeryToken },
+                { "Input.Email", _testsFixture.UsuarioEmail }, //Baseia-se no campo name do html la no formulario de cadastro
+                { "Input.Password", senhaFraca },
+                { "Input.ConfirmPassword", senhaFraca },
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            //Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            //Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains("Passwords must have at least one non alphanumeric character.", responseString);
+            Assert.Contains("Passwords must have at least one lowercase (&#x27;a&#x27;-&#x27;z&#x27;).", responseString);
+            Assert.Contains("Passwords must have at least one uppercase (&#x27;A&#x27;-&#x27;Z&#x27;).", responseString);
+        }
+
+        [Fact(DisplayName = "Realizar login com sucesso"), TestPriority(2)]
+        [Trait("Categoria", "Integração Web - Usuário")]
+        public async Task Usuario_RealizarLogin_DeveExectutarComSucesso()
+        {
+            //Arrange
+            var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Login");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>
+            {
+                { _testsFixture.AntiForgeryFieldName, antiForgeryToken },
+                { "Input.Email", _testsFixture.UsuarioEmail }, //Baseia-se no campo name do html la no formulario de cadastro
+                { "Input.Password", _testsFixture.UsuarioSenha }
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            //Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            //Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
             postResponse.EnsureSuccessStatusCode();
             Assert.Contains($"Hello {_testsFixture.UsuarioEmail}!", responseString);
         }
